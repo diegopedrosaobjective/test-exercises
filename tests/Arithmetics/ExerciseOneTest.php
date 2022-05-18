60f9af360db65de45bc748692d8ac0d1cf3ae8e8
Diego Pedrosa <diego.pedrosa@objective.com.br>
2022-05-18 16:25:43 -0300

    Refactoring exercise one tests

--- tests/Arithmetics/ExerciseOneTest.php
+++ tests/Arithmetics/ExerciseOneTest.php
@@ -2,47 +2,34 @@
 
 namespace Tests\Arithmetics;
 
+use Exercises\Arithmetics\ExerciseOne;
 use Exercises\Arithmetics\NumbersMod;
 use PHPUnit\Framework\TestCase;
 
 class ExerciseOneTest extends TestCase {
 
+    private ExerciseOne $exerciseOne;
+
+    function setUp(): void
+    {
+        $this->exerciseOne = new ExerciseOne();
+    }
+
     public function testSumOfAllNumbersTillThousandDivisibleByThreeAndFive () {
-        $i = 0;
-        $numbersArray = [];
 
-        for ($i; $i < 1000; $i++) {
-            $divisibleBy = new NumbersMod($i);
-            $numbersArray[] = $divisibleBy->isDivisibleByThreeAndFive() ? $i : 0 ;
-        }
+        $this->assertEquals(33165, $this->exerciseOne->sumOfAllNumbersTillThousandDivisibleByThreeAndFive());
 
-        $result = array_sum($numbersArray);
-        $this->assertEquals(33165, $result);
     }
 
     public function testSumOfAllNumbersTillThousandDivisibleByThreeOrFive () {
-        $i = 0;
-        $numbersArray = [];
 
-        for ($i; $i < 1000; $i++) {
-            $divisibleBy = new NumbersMod($i);
-            $numbersArray[] = $divisibleBy->isDivisibleByThreeOrFive() ? $i : 0 ;
-        }
+        $this->assertEquals(233168, $this->exerciseOne->sumOfAllNumbersTillThousandDivisibleByThreeOrFive());
 
-        $result = array_sum($numbersArray);
-        $this->assertEquals(233168, $result);
     }
 
     public function testSumOfAllNumbersTillThousandDivisibleByThreeOrFiveAndSeven () {
-        $i = 0;
-        $numbersArray = [];
 
-        for ($i; $i < 1000; $i++) {
-            $divisibleBy = new NumbersMod($i);
-            $numbersArray[] = $divisibleBy->isDivisibleByThreeOrFiveAndSeven() ? $i : 0 ;
-        }
+        $this->assertEquals(33173, $this->exerciseOne->sumOfAllNumbersTillThousandDivisibleByThreeOrFiveAndSeven());
 
-        $result = array_sum($numbersArray);
-        $this->assertEquals(33173, $result);
     }
 }
